ef77d64d2917370263f7cd22b99c5cf538d02ffe
Bernhard Baumrock <BernhardBaumrock@users.noreply.github.com>
2019-01-14 12:56:53 +0100

    add parameter

--- RockMigrations/0.0.1.php
+++ RockMigrations/0.0.1.php
@@ -1,11 +1,11 @@
 <?php namespace ProcessWire;
-$upgrade = function() {
+$upgrade = function(RockMigrations $rm) {
   d("--- execute upgrade 001 ---");
   d("create field001");
   d("create template001");
 }; // dont forget the semicolon!
 
-$downgrade = function() {
+$downgrade = function(RockMigrations $rm) {
   d("--- execute downgrade 001 ---");
   d("remove field001");
   d("remove template001");
